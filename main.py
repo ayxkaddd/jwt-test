9e3fce50e79ea4716cfddbe010b8ddb492034538
мама я программист <82116858+ayxkaddd@users.noreply.github.com>
2022-09-10 20:21:51 +0300

    fixed

--- main.py
+++ main.py
@@ -5,7 +5,7 @@ import jwt
 import datetime
 
 app = Flask(__name__)
-app.config['SECRET_KEY'] = "gayass"
+app.config['SECRET_KEY'] = "randomkey"
 
 
 def check_token(func):
@@ -17,38 +17,39 @@ def check_token(func):
             return jsonify({"message": "missing token"}), 403
         try:
             date = jwt.decode(token, app.config['SECRET_KEY'], algorithms="HS256")
-            print(date)
+            return jsonify({"date": f"{date}"})
+        except jwt.ExpiredSignatureError:
+            return render_template("login.html")
         except Exception as e:
-            print(f'{e=}')
-            return jsonify({"LOL": f"gay"}), 403
+            return jsonify({"exception": f"wrong token"}), 403
     return wrapped
 
 
-@app.route("/", methods=['GET', 'POST'])
+@app.route("/", methods=["GET", "POST"])
 def index():
     if not session.get("logged_in"):
         return render_template("login.html")
     else:
         return "logged_in"
 
+
 @app.route("/public")
 def public():
     return "public page"
 
 
+@app.route("/auth", methods=["GET"])
 @check_token
-@app.route("/auth")
-def problem():
-    return "u can see this page only with token"
+def private():
+    return "null"
 
-
-@app.route("/login", methods=['POST'])
+@app.route("/login", methods=["POST"])
 def login():
     if request.form['username'] and request.form['password'] == "password":
         session["logged_in"] = True
         token = jwt.encode({
             'user': request.form["username"],
-            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=60)
+            'exp': datetime.datetime.utcnow() + datetime.timedelta(seconds=600)
         },
         app.config['SECRET_KEY'])
         return jsonify({'token': token})
@@ -59,4 +60,3 @@ def login():
 if __name__ == "__main__":
     port = int(os.environ.get('PORT', 5000))
     app.run(host='0.0.0.0', port=port, debug=True)
-    # app.run(debug=True)
